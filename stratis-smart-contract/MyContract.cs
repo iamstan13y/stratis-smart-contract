de06047e280ef7889dc3bf4e55d825ce932ca9b7
iamstan13y <iamstan13y@gmail.com>
2023-04-30 12:09:18 +0200

    Add project files.

---
+++ stratis-smart-contract/MyContract.cs
@@ -0,0 +1,10 @@
+﻿using Stratis.SmartContracts;
+using System;
+
+public class MyContract : SmartContract
+{
+    public MyContract(ISmartContractState smartContractState)
+    : base(smartContractState)
+    {
+    }
+}
