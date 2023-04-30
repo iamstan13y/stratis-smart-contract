33d525ba17381920f6737dc15f2495af07ec66a5
iamstan13y <iamstan13y@gmail.com>
2023-04-30 12:45:45 +0200

    Clean-up.

---
+++ stratis-smart-contract/HelloWorld.cs
@@ -0,0 +1,23 @@
+﻿using Stratis.SmartContracts;
+using System;
+
+public class HelloWorld : SmartContract
+{
+    public HelloWorld(ISmartContractState smartContractState)
+    : base(smartContractState)
+    {
+    }
+
+    private string Greeting
+    {
+        get
+        {
+            return this.PersistentState.GetString("Greeting");
+        }
+        set
+        {
+            this.PersistentState.SetString("Greeting", value);
+        }
+    }
+    public string SayHello() => Greeting;
+}
